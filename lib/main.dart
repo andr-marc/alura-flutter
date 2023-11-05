180ec2217546ce28f31d6ad9cca51be9d95ece03
André Marc <andreluis0015@gmail.com>
2023-11-04 21:45:54 -0300

    + text + button

--- lib/main.dart
+++ lib/main.dart
@@ -15,62 +15,79 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                color: Colors.red,
-                width: 100,
-                height: 100,
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.red,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.cyan,
+                  width: 50,
+                  height: 50,
+                ),
+                Container(
+                  color: Colors.pinkAccent,
+                  width: 50,
+                  height: 50,
+                ),
+                Container(
+                  color: Colors.purple,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            Container(
+              color: Colors.amber,
+              height: 30,
+              width: 300,
+              child: const Text(
+                'Diamante Amarelo',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black, fontSize: 28),
               ),
-              Container(
-                color: Colors.blue,
-                width: 50,
-                height: 50,
-              )
-            ],
-          ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                color: Colors.blue,
-                width: 100,
-                height: 100,
-              ),
-              Container(
-                color: Colors.red,
-                width: 50,
-                height: 50,
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                color: Colors.cyan,
-                width: 50,
-                height: 50,
-              ),
-              Container(
-                color: Colors.pinkAccent,
-                width: 50,
-                height: 50,
-              ),
-              Container(
-                color: Colors.purple,
-                width: 50,
-                height: 50,
-              )
-            ],
-          )
-        ],
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Aperte o botão!"),
+            )
+          ],
+        ),
       ),
     );
   }
