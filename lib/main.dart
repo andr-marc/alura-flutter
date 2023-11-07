8022ecd03915616b5216007882d80af45387d821
André Marc <andreluis0015@gmail.com>
2023-11-06 22:27:01 -0300

    + stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,76 +15,120 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Container(),
+          title: const Text('Tarefas'),
+        ),
+        body: ListView(
+          children: const [
+            Task(name: 'Aprender Flutter'),
+            Task(name: 'Andar de Bike'),
+            Task(name: 'Meditar'),
+            Task(name: 'Aprender Flutter'),
+            Task(name: 'Andar de Bike'),
+            Task(name: 'Meditar'),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
+  }
+}
+
+class Task extends StatefulWidget {
+  final String name;
+  const Task({
+    super.key,
+    required this.name,
+  });
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int level = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
           children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.red,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.blue,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.red,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.cyan,
-                  width: 50,
-                  height: 50,
-                ),
-                Container(
-                  color: Colors.pinkAccent,
-                  width: 50,
-                  height: 50,
-                ),
-                Container(
-                  color: Colors.purple,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
             Container(
-              color: Colors.amber,
-              height: 30,
-              width: 300,
-              child: const Text(
-                'Diamante Amarelo',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black, fontSize: 28),
-              ),
+              color: Colors.blue,
+              height: 140,
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Aperte o botão!"),
+            Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                      ),
+                      Container(
+                        width: 200,
+                        child: Text(
+                          widget.name,
+                          style: const TextStyle(
+                              fontSize: 24, overflow: TextOverflow.ellipsis),
+                        ),
+                      ),
+                      Container(
+                        height: 52,
+                        width: 52,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                level++;
+                              });
+                            },
+                            child: const Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Icon(Icons.arrow_drop_up),
+                                Text(
+                                  'UP',
+                                  style: TextStyle(fontSize: 12),
+                                )
+                              ],
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 200,
+                        child: LinearProgressIndicator(
+                          value: level / 10,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Nível: $level',
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ],
+                )
+              ],
             )
           ],
         ),
