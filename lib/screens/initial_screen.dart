ff1734535f07e3dacd096186fe41dbe4074637a7
André Marc <andreluis0015@gmail.com>
2023-11-21 19:58:34 -0300

    + iniciando integração com o banco de dados

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_alura/components/task.dart';
+import 'package:projeto_alura/data/task_dao.dart';
 import 'package:projeto_alura/data/task_inherited.dart';
 import 'package:projeto_alura/screens/form_screen.dart';
 
@@ -18,9 +19,65 @@ class _InitialScreenState extends State<InitialScreen> {
         leading: Container(),
         title: const Text('Tarefas'),
       ),
-      body: ListView(
+      body: Padding(
         padding: const EdgeInsets.only(top: 8, bottom: 70),
-        children: TaskInherited.of(context)!.taskList,
+        child: FutureBuilder<List<Task>>(
+            future: TaskDao().findAll(),
+            builder: (context, snapshot) {
+              List<Task>? items = snapshot.data;
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return const Center(
+                    child: Column(children: [
+                      CircularProgressIndicator(),
+                      Text('Carregando')
+                    ]),
+                  );
+                case ConnectionState.waiting:
+                  return const Center(
+                    child: Column(children: [
+                      CircularProgressIndicator(),
+                      Text('Carregando')
+                    ]),
+                  );
+                case ConnectionState.active:
+                  return const Center(
+                    child: Column(children: [
+                      CircularProgressIndicator(),
+                      Text('Carregando')
+                    ]),
+                  );
+                case ConnectionState.done:
+                  if (snapshot.hasData && items != null) {
+                    if (items.isNotEmpty) {
+                      return ListView.builder(
+                          itemCount: items.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final Task task = items[index];
+                            return task;
+                          });
+                    }
+                    return const Center(
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.error_outline,
+                            size: 128,
+                          ),
+                          Text(
+                            'Não há nenhuma tarefa',
+                            style: TextStyle(
+                              fontSize: 32,
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  }
+                  return const Text('Erro ao carregar tarefas');
+              }
+              return const Text('Erro desconhecido');
+            }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
